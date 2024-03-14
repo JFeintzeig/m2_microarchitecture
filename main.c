fa59633eddbc444880cee7263bb2bc07bad8d60a
JFeintzeig <JFeintzeig@users.noreply.github.com>
2024-03-14 17:08:16 -0400

    fsqrt throughput, add in some integer math, still fast

--- main.c
+++ main.c
@@ -12,22 +12,122 @@ static const event_alias profile_events[] = {
     }},
     { "stalls", {
       "MAP_STALL",
+    }},
+    { "fp", {
+      "MAP_SIMD_UOP",
+    }},
+    { "int", {
+      "MAP_INT_UOP",
     }}
 };
 
-void profile_10(void) {
-  volatile double x = 1000.0;
+float sqrt_serial_10(void) {
+  float x = 1000.0;
   for (int i = 0; i < 100000; i++) {
-    x = sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(x))))))))));
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
   }
+  return x;
 }
 
-void profile_20(void) {
-  volatile double x = 1000.0;
+float sqrt_serial_20(void) {
+  float x = 1000.0;
   for (int i = 0; i < 100000; i++) {
-    x = sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(sqrt(x))))))))))))))))))));
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
+    x = sqrt(x);
   }
+  return x;
 }
+
+float sqrt_parallel(void) {
+  float a = 1000.0;
+  float b = 1000.0;
+  float c = 1000.0;
+  float d = 1000.0;
+  float e = 1000.0;
+  float f = 1000.0;
+  float g = 1000.0;
+  float h = 1000.0;
+
+  for (int i=0; i < 100000; i++) {
+    a = sqrt(a);
+    b = sqrt(b);
+    c = sqrt(c);
+    d = sqrt(d);
+    e = sqrt(e);
+    f = sqrt(f);
+    g = sqrt(g);
+    h = sqrt(h);
+  }
+
+  return a + b + c + d + e + f + g + h;
+}
+
+float sqrt_parallel_wint(void) {
+  float a = 1000.0;
+  float b = 1000.0;
+  float c = 1000.0;
+  float d = 1000.0;
+  float e = 1000.0;
+  float f = 1000.0;
+  float g = 1000.0;
+  float h = 1000.0;
+
+  int x = 5;
+  int x1 = 5;
+  int x2 = 5;
+  int x3 = 5;
+  int x4 = 5;
+  int y = 5;
+  int z = 5;
+
+  for (int i=0; i < 100000; i++) {
+    a = sqrt(a);
+    b = sqrt(b);
+    c = sqrt(c);
+    d = sqrt(d);
+    e = sqrt(e);
+    f = sqrt(f);
+    g = sqrt(g);
+    h = sqrt(h);
+    x = x+x;
+    x1 = x1+x1;
+    x2 = x2 + x2;
+    x3 = x3 + x3;
+    x4 = x4 + x4;
+    y = y+y;
+    z = z+z;
+  }
+
+  return a + b + c + d + e + f + g + h + (float)x + (float)y + (float)z + (float)x1 + (float)x2 + (float)x3 + (float)x4;
+}
+
 int main(int argc, const char * argv[]) {
   int ret = 0;
 
@@ -54,7 +154,7 @@ int main(int argc, const char * argv[]) {
       return 1;
   }
   // code to be measured
-  profile_10();
+  float x = sqrt_serial_10();
 
   // get counters after
   if ((ret = get_thread_counters(0, KPC_MAX_COUNTERS, counters_1))) {
@@ -79,7 +179,7 @@ int main(int argc, const char * argv[]) {
       return 1;
   }
   // code to be measured
-  profile_20();
+  float y = sqrt_serial_20();
 
   // get counters after
   if ((ret = get_thread_counters(0, KPC_MAX_COUNTERS, counters_1))) {
@@ -95,8 +195,61 @@ int main(int argc, const char * argv[]) {
       u64 val = counters_1[idx] - counters_0[idx];
       printf("%14s: %llu\n", alias->alias, val);
   }
+
+  /* PROFILE 20 */
+
+  // get counters before
+  if ((ret = get_thread_counters(0, KPC_MAX_COUNTERS, counters_0))) {
+      printf("Failed get thread counters before: %d.\n", ret);
+      return 1;
+  }
+
+  // code to be measured
+  float z = sqrt_parallel();
+
+  // get counters after
+  if ((ret = get_thread_counters(0, KPC_MAX_COUNTERS, counters_1))) {
+      printf("Failed get thread counters after: %d.\n", ret);
+      return 1;
+  }
+
+  // result
+  printf("For 100,000 loops of 8-wide:\n");
+  for (usize i = 0; i < ev_count; i++) {
+      const event_alias *alias = profile_events + i;
+      usize idx = counter_map[i];
+      u64 val = counters_1[idx] - counters_0[idx];
+      printf("%14s: %llu\n", alias->alias, val);
+  }
+
+  // get counters before
+  if ((ret = get_thread_counters(0, KPC_MAX_COUNTERS, counters_0))) {
+      printf("Failed get thread counters before: %d.\n", ret);
+      return 1;
+  }
+
+  // code to be measured
+  float z1 = sqrt_parallel_wint();
+
+  // get counters after
+  if ((ret = get_thread_counters(0, KPC_MAX_COUNTERS, counters_1))) {
+      printf("Failed get thread counters after: %d.\n", ret);
+      return 1;
+  }
+
+  // result
+  printf("For 100,000 loops of 8-wide:\n");
+  for (usize i = 0; i < ev_count; i++) {
+      const event_alias *alias = profile_events + i;
+      usize idx = counter_map[i];
+      u64 val = counters_1[idx] - counters_0[idx];
+      printf("%14s: %llu\n", alias->alias, val);
+  }
+
   // stop counting
   stop_counting();
+
+  printf("%3.3f %3.3f %3.3f %3.3f\n", x, y, z, z1);
 
   // TODO: free memory
   return 0;
